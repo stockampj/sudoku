6f0ece7c214c4c1fb873b93f205eb6c8428606d9
joel stockamp and sofia giordano <joel+sofia@>
2019-09-04 14:47:27 -0700

    run two successful tests

--- spec/player-spec.js
+++ spec/player-spec.js
@@ -1,10 +1,18 @@
 import { Game} from './../src/sudoku.js';
 
 describe ('Game', function(){
-  it('should check if user input is a number between 1-9', function(){
+  it('should check if new game creates an array', function(){
     var testGame = new Game()
-      expect(testGame.row).toEqual([5, 3, "", "", 7, "", "", "", ""])
-
+      expect(testGame.row1).toEqual([5, 3, "", "", 7, "", "", "", ""])
   });
-
 });
+
+
+
+ describe ('Game', function(){
+   it('should push unallowed values into teh failureLog array', function(){
+     var testGame = new Game();
+     expect(testGame.numbOnly()).toEqual("PASS")
+   });
+
+ });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,13 +1,21 @@
 export function Game() {
-  this.row = [5, 3, "", "", 7, "", "", "", ""];
+  this.row1 = [5, 3, "", "", 7, "", "", "", ""];
 
 }
 
-Game.prototype.numbOnly = function() {
-  myGame.row.forEach(function(num){
-    if (num !== ( 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 )){
-      return false;
-    }
-  });
 
+Game.prototype.numbOnly = function() {
+  var failureLog = [];
+  var allowedValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, ""];
+
+  for (var i = 0; i < 9; i++) {
+    if (allowedValues.indexOf(this.row1[i]) < 0) {
+      failureLog.push([i]);
+    }
+  }
+  if (failureLog.length > 0) {
+    return failureLog;
+  } else {
+    return "PASS";
+  }
 }
